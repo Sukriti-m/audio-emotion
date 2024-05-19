307bae47f4a0881199e5a7074b1dcc70c44e604a
Sukriti-m <sukritimaurya.fzd@gmail.com>
2024-05-19 14:15:39 +0530

    h5 format

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ app = Flask(__name__)
 
 # Load the model directly
 model = tf.keras.models.load_model('saved_model/model_audio')
+model.save('model_audio.h5', save_format='h5')
+model = tf.keras.models.load_model('model_audio.h5')
+
 
 def extract_features(audio_file):
     y, sr = librosa.load(audio_file, sr=None)
@@ -16,9 +19,11 @@ def extract_features(audio_file):
     mfccs = np.mean(mfccs.T, axis=0)
     return mfccs
 
+
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({'message': 'Server is running successfully'}), 200
+
 
 @app.route('/predict-emotion', methods=['POST'])
 def predict_emotion():
@@ -27,18 +32,18 @@ def predict_emotion():
 
     file = request.files['audio']
     filename = secure_filename(file.filename)
-    
+
     # Load the audio file from memory
     audio_data = file.read()
     audio_file = io.BytesIO(audio_data)
-    
+
     features = extract_features(audio_file)
     features = np.expand_dims(features, axis=0)
     features = np.expand_dims(features, axis=-1)
-    
+
     prediction = model.predict(features)
     emotion = np.argmax(prediction)
-    
+
     emotion_map = {
         0: 'Anger',
         1: 'Disgust',
@@ -48,9 +53,10 @@ def predict_emotion():
         5: 'Sadness',
         6: 'Neutral'
     }
-    
+
     result = emotion_map.get(emotion, 'Unknown')
     return jsonify({'emotion': result})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
